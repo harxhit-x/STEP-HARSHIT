4866161b843ca4bca35a18f728a13aaed8372053
harxhit-x <harshitambati@gmail.com>
2026-02-17 10:45:23 +0530

    Add files via upload

---
+++ java_uc2.java
@@ -0,0 +1,13 @@
+public class java_uc2 {
+    public static void main(String[] args) {
+
+        System.out.println("   *     **   **      **");
+        System.out.println(" *   *   *     * *     *  *     ");
+        System.out.println("*     *  *     * *     *  *     ");
+        System.out.println("*     *  **    **       *   ");
+        System.out.println("*     *  *         *              * ");
+        System.out.println(" *   *   *         *         *   ** ");
+        System.out.println("   *     *         *           **   ");
+
+    }
+}
